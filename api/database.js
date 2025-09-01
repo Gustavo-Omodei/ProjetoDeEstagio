bd2a18e02e28c8e8a48e3fd1884e60e3881d9fb9
Gustavo-Omodei <140767528+Gustavo-Omodei@users.noreply.github.com>
2025-09-01 16:10:50 -0300

    gitignore

--- api/database.js
+++ api/database.js
@@ -4,18 +4,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// const sequelize = new Sequelize("postgres", "postgres", "senha", {
-//   host: "db.xphuktdviweljyxqhiqv.supabase.co",
-//   port: 5432,
-//   dialect: "postgres",
-// });
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
     ssl: false,
   },
-  logging: false, // Disable logging; default: console.log
+  logging: false,
 });
 
 export default sequelize;
